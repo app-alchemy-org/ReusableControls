b51588906f2f3105113570dd65eee74731ad9033
Christian Strydom <christian.strydom@gmail.com>
2025-04-22 15:37:37 +0200

    Adjust styling to fit SmartThings app and add images

--- ReusableControlsTest/Platforms/Android/MainActivity.cs
+++ ReusableControlsTest/Platforms/Android/MainActivity.cs
@@ -1,11 +1,21 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
 
 namespace ReusableControlsTest
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            Window?.SetStatusBarColor(Color.FromArgb("#385C9B").ToAndroid());
+            Window?.SetNavigationBarColor(Color.FromArgb("#385C9B").ToAndroid());
+
+            base.OnCreate(savedInstanceState);
+
+
+        }
     }
 }
--- ReusableControlsTest/ViewModels/HomeViewModel.cs
+++ ReusableControlsTest/ViewModels/HomeViewModel.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using ReusableControlsTest.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReusableControlsTest.ViewModels
 {
-    internal class HomeViewModel
+    internal class HomeViewModel : BaseViewModel
     {
+        public HomeViewModel()
+        {
+            DeviceList = new ObservableCollection<DeviceModel>
+            {
+                new DeviceModel
+                {
+                    Title = "Living Room",
+                    Name = "Sammy",
+                    Status = "Offline",
+                    IconUri = "tv.png"
+                },
+                new DeviceModel
+                {
+                    Title = "Bedroom 1",
+                    Name = "Aircon",
+                    Status = "Off",
+                    IconUri = "aircon.png"
+                }
+            };
+        }
+
+        private ObservableCollection<DeviceModel> _deviceList = [];
+        public ObservableCollection<DeviceModel> DeviceList
+        {
+            get => _deviceList;
+            set
+            {
+                if (_deviceList != value)
+                {
+                    _deviceList = value;
+                    OnPropertyChanged(nameof(DeviceList));
+                }
+            }
+        }
     }
 }
